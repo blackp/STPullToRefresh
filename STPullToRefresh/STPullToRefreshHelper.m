18409996890ac36f41c8cab104087c50fa96ca9b
Philip Blackwell <p.blackwell@gmail.com>
2015-01-08 11:03:39 +1100

    Fix small visual glitch on load
    Occasionally the refresh view would move down by a few points.
    
    Not totally sure, but I think the problem was a slight difference in animation
    curves for contentOffset and contentInset. We can account for this by offsetting
    the scrollViewOriginY.
    
    Also removed unused offsetY.
    
    Some edge cases remain (scrolling again after loading), but enough time spent
    here for now.

--- STPullToRefresh/STPullToRefreshHelper.m
+++ STPullToRefresh/STPullToRefreshHelper.m
@@ -88,16 +88,14 @@
     STPullToRefreshState const state = _state;
 
     if (object == scrollView) {
-        CGFloat offsetY = 0;
-        switch (state) {
-            case STPullToRefreshStateLoading:
-                offsetY = viewHeight;
-                break;
-            default:
-                break;
-        }
         CGFloat const scrollViewOriginY = contentOffset.y + contentInset.top;
-        CGFloat const viewOriginY = MIN(-viewHeight, scrollViewOriginY);
+        CGFloat viewOriginY;
+        if (state == STPullToRefreshStateIdle) {
+            viewOriginY = MIN(-viewHeight, scrollViewOriginY);
+        } else {
+            viewOriginY = (-viewHeight < scrollViewOriginY) ? scrollViewOriginY - viewHeight : scrollViewOriginY;
+        }
+
         CGFloat viewVisibility;
         switch (state) {
             case STPullToRefreshStateIdle:
